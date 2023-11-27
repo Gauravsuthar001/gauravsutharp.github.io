f9adb642728bae22749f76a41468f18a435beb6d
Gaurav Suthar <120327163+Gauravsuthar001@users.noreply.github.com>
2023-11-28 01:58:32 +0530

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,4 +1,1 @@
-// JavaScript to toggle the menu
-document.getElementById("hamburger").addEventListener("click", function() {
-    document.getElementById("navLinks").classList.toggle("active");
-});
+
